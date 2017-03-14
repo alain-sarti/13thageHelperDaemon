2c3e695a71e6f88dcb262240fbf971dac05a3674
Alain Sarti <alain.sarti@gmail.com>
2017-03-14 21:10:35 +0100

    more e2e tests. Not much to show today, but testing alert inputs in ionic is not easy … and it doesn’t work all the time, yet :/

--- e2e/initiative-tracker.e2e-spec.ts
+++ e2e/initiative-tracker.e2e-spec.ts
@@ -1,15 +1,38 @@
-import {browser} from "protractor";
+import {browser, element, by} from "protractor";
 import {openInitiativeTrackerPage} from "./utils";
 
 describe("InitiativeTrackerPage", () => {
+    let addMasterBtn = element(by.buttonText("Add Master Slot"));
+    let addPlayerBtn = element(by.buttonText("Add Player Slot"));
 
     beforeEach(() => {
         browser.get("");
+        openInitiativeTrackerPage()
     });
 
     it("should have a title", () => {
-        openInitiativeTrackerPage().then(() => {
-            expect(browser.getTitle()).toEqual("Ini");
-        });
+        expect(browser.getTitle()).toEqual("Ini");
+    });
+
+    it("should add a GM entry", () => {
+        addMasterBtn.click();
+        let input = element(by.css('[placeholder="ini slot"]'));
+        input.sendKeys("15");
+        browser.driver.sleep(400);
+        let save = element(by.buttonText("Save"));
+        save.click();
+        let entry = element(by.css(".ion-md-nuclear"));
+        expect(entry.isPresent()).toBeTruthy();
+    });
+
+    it("should add a Player entry", () => {
+        addPlayerBtn.click();
+        let input = element(by.css('[placeholder="ini slot"]'));
+        input.sendKeys("15");
+        browser.driver.sleep(400);
+        let save = element(by.buttonText("Save"));
+        save.click();
+        let entry = element(by.css(".ion-md-contacts"));
+        expect(entry.isPresent()).toBeTruthy();
     });
 });
